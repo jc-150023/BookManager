dfeba0b7b40b5875c9f0b7bdf02dba10bb368baf
隆成 平井 <150023@jc-21.jp>
2017-12-18 12:44:13 +0900

    プロジェクト ファイルを追加します。

---
+++ BookManager/BookManager/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+//using ZXing.Mobile;
+//using ZXing.Net.Mobile.Forms;
+
+namespace BookManager
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Folder(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new FolderMakePage());
+        }
+    }
+}
